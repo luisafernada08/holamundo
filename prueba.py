e8110c06c67229f6cf5e2b2962a5d09497b6d9de
luisafernada08 <luisagoyeneche09@gmail.com>
2024-09-11 16:45:34 -0500

    se agrego un muevo archivo.py

--- prueba.py
+++ prueba.py
@@ -1,4 +1,10 @@
-print("hola mundo \n")
+print("hola mundo \n") # este limea implime en consola la palabra hola
 print("segundo regon")
 import time
-time.sleep(10)
+time.sleep(3)
+print(type ("hola")) #la funcion type me dice que tipo de datos es 
+print(type(4))
+print(type(5.5))
+"""estoy aprendiendo
+algo 
+muy nuevo"""
